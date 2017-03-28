7ea664ddea0404724c23c803ba73dd5797446a13
jongjoo <jjongju8790@gmail.com>
2017-03-29 01:49:28 +0900

    Express 3_29

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ app.use(express.static('public'));
 app.get('/',function(req,res){
     res.send('Hello World page');
 });
+app.get('/dynamic',function(req,res){
+  var lis = '';
+  for(var i=0;i<5;i++){
+    lis = lis + '<li>coding</li>'
+  }
+  var time = Date();
+  var output =`<!DOCTYPE html>
+  <html lang="ko">
+  <head>
+  <title>jj</title>
+  </head>
+  <body>
+
+  </body>
+  hello World.
+  <ul>
+  ${lis}
+  </ul>
+  ${time}
+  </html>
+`;
+  res.send(output);
+});
 app.get('/route',function(req,res){
     res.send('Hello Router,<img src="/test.jpeg"');
 });
